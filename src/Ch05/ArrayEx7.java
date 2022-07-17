28796fd600183b9a4e879625e43cc64e54e06ad4
Sion Noh <shtldhs123@ajou.ac.kr>
2022-07-17 11:33:07 +0900

    배열 무작위로 섞기

---
+++ src/Ch05/ArrayEx7.java
@@ -0,0 +1,25 @@
+package Ch05;
+
+public class ArrayEx7 {
+    public static void main(String[] args) {
+        int[] numArr = new int[10];
+
+        for (int i = 0; i < numArr.length; i++) {
+            numArr[i] = i;
+            System.out.printf("%d ", numArr[i]);
+        }
+        System.out.println();
+
+        // 섞는 거를 100번 반복 (위치 바꾸는 작업)
+        for (int i = 0; i < 100; i++) {
+            int n = (int) (Math.random() * 10); // 0~9 중 랜덤 값
+            // 0번째와 n번째(랜덤 선택) 값을 스왑
+            int tmp = numArr[0];
+            numArr[0] = numArr[n];
+            numArr[n] = tmp;
+        }
+        for (int i = 0; i < numArr.length; i++) {
+            System.out.printf("%d ", numArr[i]);
+        }
+    }
+}
